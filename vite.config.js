3406bc32a6b7c889cb3a94d91d19e43935297ed9
pathigarilinux-lang <pathigari.linux@gmail.com>
2025-11-20 09:18:49 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Increase the warning limit to 1000kb (1MB) to stop the warning
+    chunkSizeWarningLimit: 1000,
+  }
 })
