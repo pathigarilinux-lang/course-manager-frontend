16299498789bf134f6abe2b2c2e0328ca588498f
pathigarilinux-lang <pathigari.linux@gmail.com>
2025-12-22 15:40:47 +0530

    Update DiningLayout.jsx

--- src/DiningLayout.jsx
+++ src/DiningLayout.jsx
@@ -3,14 +3,6 @@ import MaleDiningLayout from './MaleDiningLayout';
 import FemaleDiningLayout from './FemaleDiningLayout';
 
 export default function DiningLayout({ gender, ...props }) {
-  // STRICT LOGIC: 
-  // If gender starts with 'F' or 'f' -> Load Female File
-  // Otherwise -> Load Male File
   const isFemale = (gender || '').toLowerCase().startsWith('f');
-
-  if (isFemale) {
-    return <FemaleDiningLayout {...props} />;
-  }
-  
-  return <MaleDiningLayout {...props} />;
+  return isFemale ? <FemaleDiningLayout {...props} /> : <MaleDiningLayout {...props} />;
 }
