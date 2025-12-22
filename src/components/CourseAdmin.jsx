d2502b32416f45533644e96dcab07e86e38c55cd
pathigarilinux-lang <pathigari.linux@gmail.com>
2025-12-22 23:23:51 +0530

    Update CourseAdmin.jsx

--- src/components/CourseAdmin.jsx
+++ src/components/CourseAdmin.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Upload, Database, Save, FileText, Download, Trash2, Calendar, Search, PlusCircle, Archive } from 'lucide-react';
 import { API_URL, styles } from '../config';
 
